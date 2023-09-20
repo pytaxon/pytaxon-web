8968bdd39dcea34a1548ddb25ffd6620b0c8513e
Ninniet5670 <marconeto3000@gmail.com>
2023-09-20 00:51:38 -0300

    two buttons to different routes added

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ def homepage1():
     return render_template('index1.html')
     
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
+@app.route('/normalize_taxonomies', methods=['POST'])
+def normalize_taxonomies():
     uploaded_file = request.files['file']
 
     if uploaded_file.filename != '':
@@ -22,6 +22,19 @@ def upload_file():
         return 'Arquivo enviado com sucesso.'
     else:
         return 'Nenhum arquivo enviado.'
+    
+
+@app.route('/normalize_lineage', methods=['POST'])
+def normalize_lineage():
+    uploaded_file = request.files['file']
+
+    if uploaded_file.filename != '':
+        # Salvar o arquivo em algum local no servidor ou processá-lo conforme necessário
+        uploaded_file.save(uploaded_file.filename)
+        return 'Arquivo enviado com sucessoo.'
+    else:
+        return 'Nenhum arquivo enviadoo.'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
