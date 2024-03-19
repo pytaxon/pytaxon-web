90c095c0bed46f145e7f372f5f77a7abd5a65782
Ninniet5670 <marconeto3000@gmail.com>
2024-03-18 23:07:26 -0300

    download button functioning propperly now

--- Homepage.py
+++ Homepage.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
 st.title('Pytaxon')
+
+with open('files/pytaxon_gui.rar', 'rb') as arq:
+    st.download_button('Baixar Pytaxon GUI', data=arq, file_name='pytaxon_gui.rar')
