ba8a9d333f444a78511c4626b39aa5a843f6931e
Ninniet5670 <marconeto3000@gmail.com>
2024-03-28 11:35:25 -0300

    zip file added

--- Homepage.py
+++ Homepage.py
@@ -3,4 +3,7 @@ import streamlit as st
 st.title('Pytaxon')
 
 with open('files/pytaxon_gui.rar', 'rb') as arq:
-    st.download_button('Baixar Pytaxon GUI', data=arq, file_name='pytaxon_gui.rar')
+    st.download_button('Baixar Pytaxon-GUI.rar', data=arq, file_name='pytaxon_gui.rar')
+
+with open('files/pytaxon_gui.zip', 'rb') as arq:
+    st.download_button('Baixar Pytaxon-GUI.zip', data=arq, file_name='pytaxon_gui.zip')
